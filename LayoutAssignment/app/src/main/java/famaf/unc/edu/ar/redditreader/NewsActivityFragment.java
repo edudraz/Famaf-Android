0ef3eaa1f08bbb3280a4ac12268f01286342977c
edudraz <edudraz@gmail.com>
2016-09-30 01:34:38 -0300

    Lab Terminado

---
+++ LayoutAssignment/app/src/main/java/famaf/unc/edu/ar/redditreader/NewsActivityFragment.java
@@ -0,0 +1,24 @@
+package famaf.unc.edu.ar.redditreader;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import famaf.unc.edu.ar.activitiesassignment.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NewsActivityFragment extends Fragment {
+
+    public NewsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_news, container, false);
+    }
+}
